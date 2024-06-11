787bdb70e330401258f5613a4d764094ef734781
Ajay Pradhan <dev.ajaypradhan@gmail.com>
2024-06-11 23:43:43 +0530

    done

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
-import "./App.css";
+import Body from "./assets/Components/Body";
+import Footer from "./assets/Components/Footer";
+import Header from "./assets/Components/Header";
 
 function App() {
   return (
     <>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <div className="min-h-screen px-6 py-3 bg-amber-50 flex flex-col gap-16 md:px-10 md:py-7 md:justify-between lg:px-16 lg:justify-between">
+        <Header />
+        <Body />
+        <Footer />
+      </div>
     </>
   );
 }
---
+++ src/assets/Components/Header.jsx
@@ -0,0 +1,19 @@
+import { FaBars } from "react-icons/fa6";
+
+const Header = () => {
+  return (
+    <div className="flex justify-between">
+      <div className="flex items-center justify-center gap-2">
+        <img src="./assets/Logo.svg" alt="logo" />
+        <button className="bg-gradient-to-r from-orange-400 to-red-400 text-xs text-white px-2.5 py-1.5 rounded-2xl">
+          Hosterr is hiring
+        </button>
+      </div>
+      <div>
+        <FaBars />
+      </div>
+    </div>
+  );
+};
+
+export default Header;
